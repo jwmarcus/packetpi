3749170fbabe099ae75381e135656cc62ff77377
John Marcus III <j3@jwmarcus.com>
2018-05-29 17:19:48 -0400

    Adding basic menu system and polling

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ MENU_POS = 0
 MENU_CMT = 1
 MENU_SPD = 2
 
+SCREEN_TYPES = [MENU_POS, MENU_CMT, MENU_SPD]
+
 MENU_OPTIONS = [{'text': "POS"},
                 {'text': "CMT"},
                 {'text': "SPD"}]
 
-# Initialize the LCD using the pins
-lcd = LCD.Adafruit_CharLCDPlate()
+LCD_BUTTONS = ((LCD.SELECT, 0, (1,1,1)),
+               (LCD.LEFT,   1, (1,0,0)),
+               (LCD.UP,     2, (0,0,1)),
+               (LCD.DOWN,   3, (0,1,0)),
+               (LCD.RIGHT,  4, (1,0,1)))
+
 
 def read_packets(logfile):
     packets = []
@@ -45,7 +51,8 @@ def parse_packet(packet):
     try:
         parsed_packet = aprslib.parse(f_packet)
     except (aprslib.exceptions.UnknownFormat):
-        print('INFO: Undecodable Message: {}'.format(f_packet))
+        # print('INFO: Undecodable Message: {}'.format(f_packet))
+        pass
     return parsed_packet
 
 def format_packet(packet):
@@ -80,6 +87,12 @@ def get_lcd_message(packet, index, screen_type):
 
     return lcd_message
 
+def update_lcd(packet, index, screen_type, lcd):
+    formatted_message = get_lcd_message(packet, index, screen_type)
+    lcd.clear()
+    lcd.message(formatted_message)
+    time.sleep(0.15) # slight de-bounce?
+
 def format_lat_long(lat, long):
     pos_ns = "n" if lat >= 0 else "s"
     pos_we = "w" if long <= 0 else "e"
@@ -109,19 +122,68 @@ def refresh_packets():
             packets.append(parse_packet(packet))
     return packets
 
+def get_button(lcd):
+    button_num = None
+    for button in LCD_BUTTONS:
+        if lcd.is_pressed(button[0]):
+            button_num = button[0]
+    return button_num
+
+def update_indexes(button_num, packet_index, page_index, packet_len, page_len):
+    if button_num is LCD.UP:
+        packet_index = (packet_index + 1) % packet_len
+    elif button_num is LCD.DOWN:
+        packet_index = (packet_index - 1) % packet_len
+    elif button_num is LCD.RIGHT:
+        page_index = (page_index + 1) % page_len
+    elif button_num is LCD.LEFT:
+        page_index = (page_index - 1) % page_len
+    return (packet_index, page_index)
+
+def blink_alert(lcd, color1, color2, iterations, speed):
+    for _ in range(iterations):
+        lcd.set_color(*color2)
+        time.sleep(speed)
+        lcd.set_color(*color1)
+        time.sleep(speed)
 
 def main():
-    # every x seconds check for new packets and update packet table
-    # every x seconds flip display between page1 and page2
-    # contunuously check for a button press, debounce every 0.25sec
-
+    # Initialize the LCD using the pins
+    lcd = LCD.Adafruit_CharLCDPlate()
+    lcd.set_color(1, 0, 0)
+    # Get first packets
     packets = refresh_packets()
 
-    for index, packet in enumerate(packets):
-        for message_type in [MENU_SPD]: # MENU_POS, MENU_CMT,
-            lcd.clear()
-            lcd.message(get_lcd_message(packet, index, message_type))
-            time.sleep(2)
+
+    # Menu system
+    packet_index = 0 # Index of which message to display
+    page_index = 0   # Index of which page of data to dispay
+    refresh_rate = 6
+    start_time = int(round(time.time()))
+
+    while True:
+        # Constantly check for button events
+        button_num = get_button(lcd)
+        if button_num is not None:
+            packet_index, page_index = update_indexes(
+                    button_num,
+                    packet_index,
+                    page_index,
+                    len(packets),
+                    len(SCREEN_TYPES)
+            )
+            update_lcd(packets[packet_index], packet_index, SCREEN_TYPES[page_index], lcd)
+
+        # Refresh the display every refresh_rate seconds
+        current_time = int(round(time.time()))
+        if current_time % start_time == 4:
+            start_time = current_time
+            last_pack_len = len(packets)
+            packets = refresh_packets()
+            if len(packets) > last_pack_len:
+                last_pack_len = len(packets)
+            blink_alert(lcd, ((1,0,0)), ((0,1,0)), 2, 0.25)
+
 
 if __name__ == "__main__":
     main()
